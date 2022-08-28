be554987faa0c5ba5ef277989d5e8b5f08037c40
israelrodrigues01 <ciceroisrael428@gmail.com>
2022-08-28 16:20:17 -0300

    Crio o form e e functions ao click do button

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
-console.log("Hello World")
+const showPassword = document.querySelector('.eye-input'),
+	  inputName = document.querySelector('#name'),
+	  inputEmail = document.querySelector('#email'),
+	  inputPassword = document.querySelector('#password'),
+	  inputNomeView = document.querySelector('#nome_view'),
+	  inputEmailViwe = document.querySelector('#email_view'),
+	  imgPerfil = document.querySelector('.imgPerfil'),
+	  inputImgPerfil = document.querySelector('#imgPerfil'),
+	  inputBtnEnviar = document.querySelector('#btn_enviar');
+
+showPassword.onclick = () =>
+{
+
+	let iconPassword = showPassword.children[0];
+	console.log(iconPassword.className)
+
+	if(inputPassword.type == "password")
+	{
+		inputPassword.type = "text";
+		iconPassword.className = "bx bx-show"
+	}
+	else if(inputPassword.type == "text")
+	{
+		inputPassword.type = "password"
+		iconPassword.className = "bx bx-hide"
+	}
+}
+
+inputBtnEnviar.onclick = () =>
+{
+
+	const nameValue = inputName.value;
+	const emailValue = inputEmail.value;
+
+	inputNomeView.innerText = nameValue;
+	inputEmailViwe.innerText = emailValue;
+	imgPerfil.src = inputImgPerfil
+
+	console.log(nameValue + " e " + emailValue)
+
+}
